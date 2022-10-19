e4cbe0cde451e773c075f6b450c9f05b52ae1cb9
grittyzavr <gritty1337@protonmail.com>
2022-10-19 17:03:26 +0300

    fixed double greeting

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
 export function askName() {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
   return name;
 }
 
