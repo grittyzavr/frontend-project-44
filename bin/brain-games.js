7ac198f660beabefecd698af8a87b6bd22598a60
grittyzavr <gritty1337@protonmail.com>
2022-10-25 10:03:30 +0300

    gcd complete

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,7 +2,9 @@
 import brainEven from './brain-even.js';
 import { askName } from '../src/cli.js';
 import brainCalc from './brain-calc.js';
+import brainGcd from './brain-gcd.js';
 
 askName();
 brainEven();
 brainCalc();
+brainGcd();
--- src/cli.js
+++ src/cli.js
@@ -5,5 +5,5 @@ const name = readlineSync.question('May I have your name? ');
 export function askName() {
   return name;
 }
-
+console.log(`Hello, ${name}!`);
 export const nameOfUser = askName();
