deeed32fac41cdcc142e91469fa685ff55bfd691
grittyzavr <gritty1337@protonmail.com>
2022-11-20 09:01:18 +0300

    refactor of 'Cognitive Complexity' issue on codeclimate.com

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -2,12 +2,10 @@ import { game, randomNumber } from '../src/index.js';
 
 const taskOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 function testPrime(n) {
-  if (n === 0) {
+  if (n === 0 || n === 1) {
     return false;
   }
-  if (n === 1) {
-    return false;
-  } if (n === 2) {
+  if (n === 2) {
     return true;
   }
   for (let x = 2; x < n; x += 1) {
